c5dcdaef4e8936939adc4a175c2f59e8120ceaf8
Fred Stark <coagmano@gmail.com>
2014-05-14 15:58:09 +1000

    broken

--- src/LDI/ProjectHubBundle/Controller/DefaultController.php
+++ src/LDI/ProjectHubBundle/Controller/DefaultController.php
@@ -10,13 +10,12 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('LDIProjectHubBundle:Default:index.html.twig');
-        $project = findProjectById($id);
     }
 
     public function projectAction($id)
     {
-        $project = findProjectById($id);
-        $users = $project->getUsers();
+        
+        
         foreach ($users as $key => $value) {
             $value->getFirstName();
         }
